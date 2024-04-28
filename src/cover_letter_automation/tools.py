aa272c6d47c509089c48136d3644f53f197793f8
Simon Podhajsky <simon.podhajsky@gmail.com>
2024-04-28 19:53:25 +0000

    Grab current datetime instead of letting LLM think it

--- src/cover_letter_automation/tools.py
+++ src/cover_letter_automation/tools.py
@@ -1,5 +1,6 @@
 """Tools for agents to call."""
 
+from datetime import UTC, datetime
 from pathlib import Path
 from typing import Annotated
 
@@ -8,7 +9,7 @@ def export_letter(
     cover_letter: Annotated[str, "Full text of the cover letter, formatted in Markdown"],
     filename: Annotated[
         str,
-        "Name of the file to save the cover letter to, preferably in the format `YY MM company_lowercase__surname.md`",
+        "Name of the file to save the cover letter to, preferably in the format `company_lowercase__surname.md`",
     ],
     path: Annotated[str, "Folder in which to save the file"] = "cover_letters/",
 ) -> str:
@@ -16,6 +17,8 @@ def export_letter(
     target_path = Path(path)
     if not target_path.exists():
         target_path.mkdir(parents=True)
+    # Prepend filename with YYYY-MM-DD:
+    filename = f"{datetime.now(tz=UTC).strftime('%Y-%m-%d')}__{filename}"
     target = target_path / filename
     target.touch()
     with target.open(mode="w") as f:
